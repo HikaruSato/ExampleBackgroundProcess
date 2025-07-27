e3c318891fa7dada1dfcf4cd3e968af533cfca6c
hs7 <sato.hikaru.dev@gmail.com>
2025-07-27 17:51:16 +0900

    Initial Commit

---
+++ ExampleBackgroundProcess/ExampleBackgroundProcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleBackgroundProcessApp.swift
+//  ExampleBackgroundProcess
+//
+//  Created by Hikaru Sato on 2025/07/27.
+//
+
+import SwiftUI
+
+@main
+struct ExampleBackgroundProcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleBackgroundProcessTests/ExampleBackgroundProcessTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleBackgroundProcessTests.swift
+//  ExampleBackgroundProcessTests
+//
+//  Created by Hikaru Sato on 2025/07/27.
+//
+
+import Testing
+@testable import ExampleBackgroundProcess
+
+struct ExampleBackgroundProcessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
